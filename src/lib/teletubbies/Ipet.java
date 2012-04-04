ad089769e5d89e6184ae33efcb27664bd3fc98df
samis <samis@Sams-laptop>
2012-04-04 15:30:23 +0100

    Added Interface (IPet)

---
+++ src/lib/teletubbies/Ipet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib.teletubbies;
+
+/**
+ *
+ * @author samis
+ */
+public interface IPet {
+    int price = 0;
+    int niceness = 0;
+    public void hug(Teletubby t);
+    public int getPrice();
+    public void setPrice(int price);
+    public int getNiceness();
+    public void setNiceness(int niceness);
+    public void play();
+    public void play(Teletubby t);
+    public void sleep();
+}
