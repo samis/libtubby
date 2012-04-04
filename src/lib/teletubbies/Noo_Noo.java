d1d32f3d3f29ccc026d0698c6c8fa317c6e52e30
samis <samis@Sams-laptop>
2012-04-04 15:42:10 +0100

    Started work on Noo Noo (Implements pet interface, provides the example for the suite.) TODO: Actually write the method bodies in Noo Noo's class.

---
+++ src/lib/teletubbies/Noo_Noo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib.teletubbies;
+
+/**
+ *
+ * @author samis
+ */
+public class Noo_Noo implements IPet {
+    public void sleep() {
+        
+    }
+    public void play(Teletubby t) {
+        
+    }
+    public void play() {
+        
+    }
+    public void setNiceness(int niceness) {
+        
+    }
+    public int getNiceness() {
+        return niceness;
+    }
+    public void setPrice(int price) {
+        
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void hug(Teletubby t) {
+        
+    }
+        
+    }
