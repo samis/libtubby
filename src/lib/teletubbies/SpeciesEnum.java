5d7a1fc5b3161f9288dc646be77f7857dec7d92e
samis <samis@Sams-laptop>
2012-04-08 21:10:24 +0100

    SpeciesEnum internationalized.

--- src/lib/teletubbies/SpeciesEnum.java
+++ src/lib/teletubbies/SpeciesEnum.java
@@ -9,8 +9,8 @@ package lib.teletubbies;
  * @author samis
  */
 public enum SpeciesEnum {
-	Po("Po"), TinkyWinky("Tinky Winky"), Dipsy("Dipsy"), Laa_Laa("Laa Laa"), Evil_Dipsy(
-			"Evil Dipsy");
+	Po(java.util.ResourceBundle.getBundle("lib/teletubbies/Bundle").getString("PO")), TinkyWinky(java.util.ResourceBundle.getBundle("lib/teletubbies/Bundle").getString("TINKY WINKY")), Dipsy(java.util.ResourceBundle.getBundle("lib/teletubbies/Bundle").getString("DIPSY")), Laa_Laa(java.util.ResourceBundle.getBundle("lib/teletubbies/Bundle").getString("LAA LAA")), Evil_Dipsy(
+			java.util.ResourceBundle.getBundle("lib/teletubbies/Bundle").getString("EVIL DIPSY"));
 	public String spec;
 
 	SpeciesEnum(String species) {
